006f5e8c90b22f4df211a684f1bce04a837b330d
{jeongmin26} <{ljmljm24680@gmail.com}>
2026-07-30 17:28:51 +0900

    feat: JwtProvider 토큰 검증 기능 추가 AuthServiceImpl 토큰 재발급 기능 추가 RefreshTokenValidateException 추가
    Fixed #52

--- src/main/java/com/nhnacademy/userauthapi/client/AccountClient.java
+++ src/main/java/com/nhnacademy/userauthapi/client/AccountClient.java
@@ -5,6 +5,7 @@ import com.nhnacademy.userauthapi.dto.login.LoginResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name="4iren-account", path="/api/account")
@@ -12,4 +13,7 @@ public interface AccountClient {
 
     @GetMapping("/login")
     ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request);
+
+    @GetMapping("/{userId}")
+    ResponseEntity<LoginResponse> getUser(@PathVariable("userId") String userId);
 }

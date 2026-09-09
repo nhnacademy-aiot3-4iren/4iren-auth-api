8b7c9b8419c7ef8d3352f2f4abc5a22bfea14733
shinjunsik <jun03031@gmail.com>
2026-09-09 16:31:26 +0900

    test

--- src/main/java/com/nhnacademy/userauthapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/nhnacademy/userauthapi/exception/GlobalExceptionHandler.java
@@ -16,7 +16,7 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(LoginFailException.class)
     public ResponseEntity<ErrorResponse> handleLoginFailException(LoginFailException ex) {
-        log.warn("LoginFailException: {}", ex.getMessage());
+        log.error("LoginFailException: {}", ex.getMessage());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
                 ErrorResponse.of(HttpStatus.NOT_FOUND.value(), ex.getMessage())
